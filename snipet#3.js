26e4c603f0e38ca78ce84bdb3b4677ef978cdb76
Antonio-M. MORENO-MORENO <48258193+tonimoreno-us@users.noreply.github.com>
2021-09-29 14:51:01 +0200

    Create snipet#3.js

---
+++ snipet#3.js
@@ -0,0 +1,5 @@
+/* Esta instrucción realiza la misma funcionalidad que un bucle 'for'
+    En este caso recorre los objetos seleccionados mediante XPath, que son etiquetas 'a'
+    Y extrae el atributo 'href' de cada objeto.  */
+
+[].map.call($x('//*[@id="content-wrapper"]/div/h4//a'), function(x){ return x.href;});
