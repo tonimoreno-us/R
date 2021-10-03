f089bbef47ac44d4898cfcab6f0b25cef51bcc17
Antonio-M. MORENO-MORENO <48258193+tonimoreno-us@users.noreply.github.com>
2021-10-03 13:50:12 +0200

    Delete snipet#1.js

--- snipet#1.js
+++
@@ -1,9 +0,0 @@
-/* SNIPET 1 - Obtenemos nombres de Dpto. desde : http://https://www.us.es/centros/departamentos */
-
-// Creamos variable donde se introducen los 'div' con los nombres de Dpto.
-cntnd = $x('//*[@id="block-views-block-departamentos-block-1"]/div/div/div/div[2]/div');
-
-// Bucle que va desde 0 hasta el número de Dpto. que aparecen en la página
-for(var i=0; i < cntnd.length; i++){
-    console.log(cntnd[i].textContent);
-}
